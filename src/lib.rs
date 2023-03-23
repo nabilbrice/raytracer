eead3df1b398a8059eee19606cbe6cc3d7a76477
Nabil Brice <nabilbrice@yahoo.co.uk>
2023-03-23 14:25:18 +0000

    added directional emission into raytrace function

--- src/lib.rs
+++ src/lib.rs
@@ -33,24 +33,28 @@ pub fn raytrace(ray: &Ray, scene: &Vec<Hittable>, scatter_depth: u8) -> Color {
                         .min_by(|x,y| {x.1.total_cmp(&y.1)})
                         .unwrap();
 
-    match hit_obj.material {
-        Material::Emitter{albedo} => { return albedo },
-        _ => {
-            if param != FARAWAY {
-                let scatter_loc: Vec3 = ray.position_at(param);
-                let scatter_ray: Ray = hit_obj.material.scatter(ray, &hit_obj.shape, scatter_loc);
-                let obj_relative_loc: Vec3;
-                match &hit_obj.shape {
-                    Shape::Sphere(sphere) => obj_relative_loc = (scatter_loc - sphere.centre).normalize(),
-                    Shape::Disc(disc) => obj_relative_loc = scatter_loc - disc.centre,
-                }
-                return hit_obj.material.albedo(&obj_relative_loc) * raytrace(&scatter_ray, scene, scatter_depth - 1)
+
+        if param != FARAWAY {
+            let scatter_loc: Vec3 = ray.position_at(param);
+            match hit_obj.material {
+                Material::Emitter{albedo} => {
+                    let cosine: f64 = ray.dir.dotprod(&hit_obj.shape.normal_at(scatter_loc));
+                    return albedo * cosine.abs()
+                },
+                _ => {},
             }
+            let scatter_ray: Ray = hit_obj.material.scatter(ray, &hit_obj.shape, scatter_loc);
+            let obj_relative_loc: Vec3;
+            match &hit_obj.shape {
+                Shape::Sphere(sphere) => obj_relative_loc = (scatter_loc - sphere.centre).normalize(),
+                Shape::Disc(disc) => obj_relative_loc = scatter_loc - disc.centre,
+            }
+            return hit_obj.material.albedo(&obj_relative_loc) * raytrace(&scatter_ray, scene, scatter_depth - 1)
         }
-    }
+    
 
     // Current calculation for sky color when no intersection is made
-    
+
     let t = 0.5 * (ray.dir.1 + 1.0);
 
     (1.0 - t) * Color{r: 1.0, g: 1.0, b: 1.0} + t* Color{r: 0.5, g: 0.7, b: 1.0}
