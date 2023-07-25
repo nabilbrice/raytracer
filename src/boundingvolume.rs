7b9fe5213edcdfc237570a7d1cc44e30aa01ec5a
Nabil Brice <nabilbrice@yahoo.co.uk>
2023-07-25 13:37:22 +0100

    added split on longest axis method for volumes

--- src/boundingvolume.rs
+++ src/boundingvolume.rs
@@ -1,6 +1,7 @@
 use crate::ray::Ray;
 use crate::geometry;
 
+#[derive(Debug, Clone)]
 struct Interval {
     minimum: f64,
     maximum: f64,
@@ -28,7 +29,18 @@ fn union(in1: &Interval, in2: &Interval) -> Interval {
     Interval{minimum, maximum}
 }
 
+#[derive(Debug, Clone)]
 struct BoundingBox(Interval, Interval, Interval);
+
+fn compose(bbox1: Option<&BoundingBox>, bbox2: Option<&BoundingBox>) -> Option<BoundingBox> {
+    if bbox1.is_none() {
+        return bbox2.cloned();
+    }
+    if bbox2.is_none() {
+        return bbox1.cloned();
+    }
+    Some(bbox1.unwrap().compose(bbox2.unwrap()))
+}
 
 impl BoundingBox {
     fn check_intersection(&self, ray: &Ray) -> bool {
@@ -60,6 +72,26 @@ impl BoundingBox {
         }
         2
     }
+}
+
+fn split_by_longest_axis(bblist: &mut [BoundingBox]) -> (&mut [BoundingBox], &mut [BoundingBox]) {
+    let bigbox = bblist.iter().fold( None,
+    |acc, bbox| compose(acc.as_ref(), Some(&bbox))).unwrap();
+    let axis = bigbox.longest_axis();
+
+    bblist.sort_by(|a,b| {
+        match axis {
+            0 => a.0.minimum.partial_cmp(&b.0.minimum).unwrap(),
+            1 => a.1.minimum.partial_cmp(&b.1.minimum).unwrap(),
+            2 => a.2.minimum.partial_cmp(&b.2.minimum).unwrap(),
+            _ => unreachable!("Axis is not 0, 1 or 2"),
+        }
+    });
+
+    let length = bblist.len().clone();
+    let (leftboxs, rightboxs) = bblist.split_at_mut(length/2);
+
+    (leftboxs, rightboxs)
 }
 
 impl geometry::Sphere {
@@ -127,20 +159,44 @@ mod tests {
         use crate::geometry::Shape;
         let scene: Vec<Hittable> = vec![Hittable{shape: Shape::Sphere(geometry::Sphere{centre: Vec3(0.0, 0.0, 0.0), radius: 1.0}), material: crate::materials::Material::Diffuse{albedo: crate::Color::new(0.0, 0.0, 0.0)}},
                                         Hittable{shape: Shape::Sphere(geometry::Sphere{centre: Vec3(1.0, 1.0, 1.0), radius: 1.0}), material: crate::materials::Material::Diffuse{albedo: crate::Color::new(0.0, 0.0, 0.0)}},
-                                        Hittable{shape: Shape::Sphere(geometry::Sphere{centre: Vec3(-1.0, -1.0, -1.0), radius: 1.0}), material: crate::materials::Material::Diffuse{albedo: crate::Color::new(0.0, 0.0, 0.0)}}];
+                                        Hittable{shape: Shape::Sphere(geometry::Sphere{centre: Vec3(-1.0, 3.0, -1.0), radius: 1.0}), material: crate::materials::Material::Diffuse{albedo: crate::Color::new(0.0, 0.0, 0.0)}}];
         let bboxs: Vec<BoundingBox> = scene.iter().map(|hittable| 
             match &hittable.shape {
                 Shape::Sphere(sphere) => sphere.surround(),
-                _ => panic!("Not a sphere"),
+                _ => unreachable!("Not a sphere"),
             }).collect();
-        let bigbbox = bboxs.iter().fold(BoundingBox(Interval{minimum: 0.0, maximum: 0.0}, Interval{minimum: 0.0, maximum: 0.0}, Interval{minimum: 0.0, maximum: 0.0}), 
-        |acc, bbox| acc.compose(bbox));
+        let bigbbox = bboxs.iter().fold( None, 
+        |acc, bbox| compose(acc.as_ref(), Some(&bbox))).unwrap();
 
         assert_eq!(bigbbox.0.minimum, -2.0);
         assert_eq!(bigbbox.0.maximum, 2.0);
-        assert_eq!(bigbbox.1.minimum, -2.0);
-        assert_eq!(bigbbox.1.maximum, 2.0);
+        assert_eq!(bigbbox.1.minimum, -1.0);
+        assert_eq!(bigbbox.1.maximum, 4.0);
         assert_eq!(bigbbox.2.minimum, -2.0);
         assert_eq!(bigbbox.2.maximum, 2.0);
+
+        assert_eq!(bigbbox.longest_axis(), 1); // biggest axis from -1 to 4
+    }
+
+    #[test]
+    fn test_sort_longest_axis() {
+        use crate::geometry::Shape;
+        let scene: Vec<Hittable> = vec![Hittable{shape: Shape::Sphere(geometry::Sphere{centre: Vec3(0.0, 0.0, 0.0), radius: 1.0}), material: crate::materials::Material::Diffuse{albedo: crate::Color::new(0.0, 0.0, 0.0)}},
+                                        Hittable{shape: Shape::Sphere(geometry::Sphere{centre: Vec3(1.0, 3.0, 1.0), radius: 1.5}), material: crate::materials::Material::Diffuse{albedo: crate::Color::new(0.0, 0.0, 0.0)}},
+                                        Hittable{shape: Shape::Sphere(geometry::Sphere{centre: Vec3(-1.0, 3.0, -1.0), radius: 1.0}), material: crate::materials::Material::Diffuse{albedo: crate::Color::new(0.0, 0.0, 0.0)}}];
+        let mut bboxs: Vec<BoundingBox> = scene.iter().map(|hittable|
+            match &hittable.shape {
+                Shape::Sphere(sphere) => sphere.surround(),
+                _ => unreachable!("Not a sphere"),
+            }).collect();
+     
+        let (leftboxs, rightboxs) = split_by_longest_axis(&mut bboxs);
+
+        assert_eq!(rightboxs[0].1.minimum, 1.5);
+
+        let (rightleftboxs, rightrightboxs) = split_by_longest_axis(rightboxs);
+
+        assert_eq!(rightleftboxs[0].0.minimum, -2.0);
+        
     }
 }
