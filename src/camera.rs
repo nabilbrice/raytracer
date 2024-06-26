81fb6f771cb57f32bf2ab0e7ab995ea7c16a4295
Nabil Brice <nabilbrice@yahoo.co.uk>
2024-06-26 16:37:42 +0100

    updated local

--- src/camera.rs
+++ src/camera.rs
@@ -1,72 +1,87 @@
 use crate::vector::Vec3;
-use rand::{Rng, thread_rng};
+use rand::{thread_rng, Rng};
 
 pub struct Camera {
-    pub lookat: Vec3, // focal plane location
+    pub lookat: Vec3,   // focal plane location
     pub lookfrom: Vec3, // lens location
-    pub lookup: Vec3, // up direction for the camera
+    pub lookup: Vec3,   // up direction for the camera
     pub focal_distance: f64,
     pub inv_focal_length: f64, // 1/f sets field of view, fewer divisions
     pub aperture: f64,
     pub horiz_arm: Vec3,
     pub vert_arm: Vec3,
     pub horiz_res: u32, // number of horizontal pixels
-    pub vert_res: u32, // number of vertical pixels
+    pub vert_res: u32,  // number of vertical pixels
     pub aspect_ratio: f64,
 }
 
 impl Camera {
-    pub fn build(lookat: Vec3, lookfrom: Vec3, inv_focal_length: f64, aperture: f64,
-        horiz_res: u32, vert_res: u32) -> Camera {
-            let lookup = Vec3([0.0, 1.0, 0.0]); // fiducial value, normalize first
-            let pointing_direction: Vec3 = lookat - lookfrom;
-            let focal_distance = pointing_direction.norm();
-            // upside down because flipped through aperture
-            let horiz_arm: Vec3 = lookup.cross(&pointing_direction).normalize();
-            let vert_arm: Vec3 = pointing_direction.cross(&horiz_arm).normalize();
-            let aspect_ratio: f64 = horiz_res as f64 / vert_res as f64;
-            Camera {
-                lookat, lookfrom, lookup, focal_distance, inv_focal_length, aperture,
-                horiz_arm, vert_arm,
-                horiz_res, vert_res, aspect_ratio
-            }
-
+    pub fn build(
+        lookat: Vec3,
+        lookfrom: Vec3,
+        inv_focal_length: f64,
+        aperture: f64,
+        horiz_res: u32,
+        vert_res: u32,
+    ) -> Camera {
+        let lookup = Vec3([0.0, 1.0, 0.0]); // fiducial value, normalize first
+        let pointing_direction: Vec3 = lookat - lookfrom;
+        let focal_distance = pointing_direction.norm();
+        // upside down because flipped through aperture
+        let horiz_arm: Vec3 = lookup.cross(&pointing_direction).normalize();
+        let vert_arm: Vec3 = pointing_direction.cross(&horiz_arm).normalize();
+        let aspect_ratio: f64 = horiz_res as f64 / vert_res as f64;
+        Camera {
+            lookat,
+            lookfrom,
+            lookup,
+            focal_distance,
+            inv_focal_length,
+            aperture,
+            horiz_arm,
+            vert_arm,
+            horiz_res,
+            vert_res,
+            aspect_ratio,
+        }
     }
 
     pub fn get_focus_loc(&self) -> Vec3 {
         let rng_scalars = random_in_disc();
 
         let nudged_lookfrom: Vec3 = self.lookfrom
-                            + self.aperture * rng_scalars[0]*self.horiz_arm.normalize() 
-                            + self.aperture * rng_scalars[1]*self.vert_arm.normalize(); 
+            + self.aperture * rng_scalars[0] * self.horiz_arm.normalize()
+            + self.aperture * rng_scalars[1] * self.vert_arm.normalize();
 
         return nudged_lookfrom;
     }
 
-    pub fn get_sample_loc(&self, i: u32, j:u32) -> Vec3 {
+    pub fn get_sample_loc(&self, i: u32, j: u32) -> Vec3 {
         let rng_scalars: [f64; 2] = thread_rng().gen();
 
-        let horiz_increm = 1.0/f64::from(self.horiz_res);
-        let vert_increm = 1.0/f64::from(self.vert_res);
+        let horiz_increm = 1.0 / f64::from(self.horiz_res);
+        let vert_increm = 1.0 / f64::from(self.vert_res);
         let horiz_nudge: Vec3 = (rng_scalars[0] * horiz_increm) * self.horiz_arm;
         let vert_nudge: Vec3 = (rng_scalars[1] * vert_increm) * self.vert_arm;
 
         let horiz_span = self.inv_focal_length * self.focal_distance * self.horiz_arm;
         let vert_span = self.inv_focal_length * self.focal_distance * self.vert_arm;
 
-        let grid_h_offset = -0.5 + f64::from(i)*horiz_increm;
-        let grid_v_offset = 0.5 - f64::from(j)*vert_increm;
+        let grid_h_offset = -0.5 + f64::from(i) * horiz_increm;
+        let grid_v_offset = 0.5 - f64::from(j) * vert_increm;
 
-        self.lookat + (grid_h_offset * horiz_span) + (grid_v_offset * vert_span) 
-        + horiz_nudge + vert_nudge
+        self.lookat
+            + (grid_h_offset * horiz_span)
+            + (grid_v_offset * vert_span)
+            + horiz_nudge
+            + vert_nudge
     }
-
 }
 
-fn random_in_disc() -> [f64;2] {
+fn random_in_disc() -> [f64; 2] {
     let rng_scalars: [f64; 2] = thread_rng().gen();
 
-    let radius2: f64 = rng_scalars[0]*rng_scalars[0] + rng_scalars[1]*rng_scalars[1]; // rejection condition
+    let radius2: f64 = rng_scalars[0] * rng_scalars[0] + rng_scalars[1] * rng_scalars[1]; // rejection condition
     if radius2 > 1.0 {
         return random_in_disc();
     };
@@ -80,6 +95,9 @@ mod tests {
     #[test]
     fn random_in_disc_test() {
         let point = random_in_disc();
-        assert!(point[0]*point[0] + point[1]*point[1] <= 1.0, "picked point out of disc")
+        assert!(
+            point[0] * point[0] + point[1] * point[1] <= 1.0,
+            "picked point out of disc"
+        )
     }
 }
--- src/materials.rs
+++ src/materials.rs
@@ -1,11 +1,11 @@
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
 
-use crate::rgba_to_color;
-use crate::{vector::Vec3, geometry::Shape};
 use crate::color::Color;
 use crate::ray::Ray;
-use rand::{Rng, thread_rng};
+use crate::rgba_to_color;
+use crate::{geometry::Shape, vector::Vec3};
 use image::{DynamicImage, GenericImageView};
+use rand::{thread_rng, Rng};
 use std::f64::consts::PI;
 
 use image::Rgba;
@@ -13,16 +13,25 @@ use image::Rgba;
 #[serde_with::serde_as]
 #[derive(Debug, Serialize, Deserialize)]
 pub enum Material {
-    Diffuse {albedo: Color},
-    Metal {albedo: Color, fuzz: f64},
-    Dielectric {refractive_index: f64},
+    Diffuse {
+        albedo: Color,
+    },
+    Metal {
+        albedo: Color,
+        fuzz: f64,
+    },
+    Dielectric {
+        refractive_index: f64,
+    },
     TextureMap {
         #[serde_as(as = "TextureMapFilePath")]
         map: DynamicImage,
         orient_up: Vec3,
         orient_around: Vec3,
     },
-    Emitter {albedo: Color},
+    Emitter {
+        albedo: Color,
+    },
 }
 
 fn load_image(path_to_file: &str) -> image::DynamicImage {
@@ -33,69 +42,92 @@ serde_with::serde_conv!(
     TextureMapFilePath,
     DynamicImage,
     |_map: &DynamicImage| "texturemap.jpeg",
-    |path_to_file: &str| -> Result<_, std::convert::Infallible> {Ok(load_image(path_to_file))}
+    |path_to_file: &str| -> Result<_, std::convert::Infallible> { Ok(load_image(path_to_file)) }
 );
 
 impl Material {
     pub fn albedo(&self, location: &Vec3) -> Color {
         match self {
-            Material::Diffuse{albedo: color} => *color,
-            Material::Metal{albedo: color, fuzz: _} => *color,
-            Material::Dielectric { refractive_index: _ } => Color::new(1.0, 1.0, 1.0),
-            Material::TextureMap {map: img, orient_up, orient_around} => {
+            Material::Diffuse { albedo: color } => *color,
+            Material::Metal {
+                albedo: color,
+                fuzz: _,
+            } => *color,
+            Material::Dielectric {
+                refractive_index: _,
+            } => Color::new(1.0, 1.0, 1.0),
+            Material::TextureMap {
+                map: img,
+                orient_up,
+                orient_around,
+            } => {
                 let latitude: f64 = orient_up.normalize().dotprod(&location).acos();
-                let orient_axes: (Vec3, Vec3) = (orient_around.normalize(), orient_up.normalize().cross(&orient_around.normalize()));
-                let longitude: f64 = orient_axes.0.dotprod(&location).atan2(orient_axes.1.dotprod(&location)) + PI;
+                let orient_axes: (Vec3, Vec3) = (
+                    orient_around.normalize(),
+                    orient_up.normalize().cross(&orient_around.normalize()),
+                );
+                let longitude: f64 = orient_axes
+                    .0
+                    .dotprod(&location)
+                    .atan2(orient_axes.1.dotprod(&location))
+                    + PI;
                 let texture_color: Rgba<u8> = get_texture_rgba(&img, longitude, latitude);
                 rgba_to_color(texture_color)
-            },
-            Material::Emitter{albedo: color} => *color,
+            }
+            Material::Emitter { albedo: color } => *color,
         }
     }
     pub fn scatter(&self, inc_ray: &Ray, shape: &Shape, scatter_loc: Vec3) -> Ray {
         match *self {
-            Material::Diffuse{albedo: _} => {
+            Material::Diffuse { albedo: _ } => {
                 let scatter_dir = shape.normal_at(scatter_loc) + random_vec3();
-                return Ray::new(scatter_loc, scatter_dir)
-            },
-            Material::Metal{albedo: _, fuzz: fuzziness} => {
+                return Ray::new(scatter_loc, scatter_dir);
+            }
+            Material::Metal {
+                albedo: _,
+                fuzz: fuzziness,
+            } => {
                 let scatter_normal = shape.normal_at(scatter_loc);
-                let scatter_dir: Vec3 = inc_ray.dir - 2.0 * scatter_normal.dotprod(&inc_ray.dir) * scatter_normal;
+                let scatter_dir: Vec3 =
+                    inc_ray.dir - 2.0 * scatter_normal.dotprod(&inc_ray.dir) * scatter_normal;
                 let fuzzified_dir = fuzzify(fuzziness, scatter_dir, scatter_normal);
-                return Ray::new(scatter_loc, fuzzified_dir)
-            },
-            Material::Dielectric{refractive_index: r_idx} => {
+                return Ray::new(scatter_loc, fuzzified_dir);
+            }
+            Material::Dielectric {
+                refractive_index: r_idx,
+            } => {
                 let scatter_normal = shape.normal_at(scatter_loc);
                 let inc_cos = scatter_normal.dotprod(&inc_ray.dir); // -ve the usual for most ray-tracers
-                let inc_dir_perp: Vec3 = inc_ray.dir - inc_cos*scatter_normal;
+                let inc_dir_perp: Vec3 = inc_ray.dir - inc_cos * scatter_normal;
                 let mut refract_ratio = r_idx; // default ray going from inside to outside so fewer divisions
                 let sign_inc = inc_cos.signum(); // needed for determining scattered ray parallel direction
-                if sign_inc < 0.0 {refract_ratio = 1.0/r_idx}; // refract from the outside
+                if sign_inc < 0.0 {
+                    refract_ratio = 1.0 / r_idx
+                }; // refract from the outside
                 let scatter_dir_perp = refract_ratio * inc_dir_perp;
                 let scatter_sin2: f64 = scatter_dir_perp.dotprod(&scatter_dir_perp); // no sqrt needed
 
-                if scatter_sin2 > 1.0 || schlick(inc_cos, refract_ratio)  {
+                if scatter_sin2 > 1.0 || schlick(inc_cos, refract_ratio) {
                     // total internal reflection
                     let scatter_dir: Vec3 = inc_dir_perp - inc_cos * scatter_normal;
                     return Ray::new(scatter_loc, scatter_dir);
-                }
-                else {
+                } else {
                     // refraction
                     // refracted ray goes in the same direction as inc ray so sign of cos is the same
                     let scatter_cos: f64 = sign_inc * (1.0 - scatter_sin2).sqrt();
                     let scatter_dir = scatter_dir_perp + scatter_cos * scatter_normal;
                     return Ray::new(scatter_loc, scatter_dir);
                 };
-            },
+            }
             Material::TextureMap { .. } => {
                 let scatter_dir = shape.normal_at(scatter_loc) + random_vec3();
-                return Ray::new(scatter_loc, scatter_dir)
-            },
+                return Ray::new(scatter_loc, scatter_dir);
+            }
             _ => {
                 panic!("Attempted to access scatter for Material without scattering implemented")
-            },
             }
         }
+    }
 }
 
 fn schlick(cosine: f64, r_idx: f64) -> bool {
@@ -110,7 +142,9 @@ fn fuzzify(fuzziness: f64, scatter_dir: Vec3, scatter_normal: Vec3) -> Vec3 {
     let fuzzy_dir = scatter_dir + (fuzziness * random_vec3());
     if fuzzy_dir.dotprod(&scatter_normal) > 0.0 {
         fuzzy_dir
-    } else { fuzzify(fuzziness, scatter_dir, scatter_normal) }
+    } else {
+        fuzzify(fuzziness, scatter_dir, scatter_normal)
+    }
 }
 
 fn random_vec3() -> Vec3 {
@@ -125,12 +159,16 @@ fn random_vec3() -> Vec3 {
 fn get_texture_rgba(image: &DynamicImage, longitude_rad: f64, latitude_rad: f64) -> Rgba<u8> {
     let dimensions: (u32, u32) = image.dimensions();
 
-    let (pixel_column, pixel_row): (f64, f64) =
-        (0.5 * longitude_rad / PI * (dimensions.0 as f64), latitude_rad / PI * (dimensions.1 as f64));
+    let (pixel_column, pixel_row): (f64, f64) = (
+        0.5 * longitude_rad / PI * (dimensions.0 as f64),
+        latitude_rad / PI * (dimensions.1 as f64),
+    );
 
-    image.get_pixel(pixel_column as u32 % dimensions.0, pixel_row as u32 % dimensions.1)
+    image.get_pixel(
+        pixel_column as u32 % dimensions.0,
+        pixel_row as u32 % dimensions.1,
+    )
 }
 
 #[cfg(test)]
-mod tests {
-}
+mod tests {}
